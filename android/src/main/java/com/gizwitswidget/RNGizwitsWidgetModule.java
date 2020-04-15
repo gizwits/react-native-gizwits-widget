3301a860a27fe04c058ef1998fe8f6cb4b88e8b3
william Zhang <williamzhang@williamdeMacBook-Pro.local>
2020-04-15 14:47:31 +0800

    修复安卓报错问题

--- android/src/main/java/com/gizwitswidget/RNGizwitsWidgetModule.java
+++ android/src/main/java/com/gizwitswidget/RNGizwitsWidgetModule.java
@@ -6,17 +6,17 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class RNGizWidgetManagerModule extends ReactContextBaseJavaModule {
+public class RNGizwitsWidgetModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
-  public RNGizWidgetManagerModule(ReactApplicationContext reactContext) {
+  public RNGizwitsWidgetModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
   }
 
   @Override
   public String getName() {
-    return "RNGizWidgetManager";
+    return "RNGizwitsWidgetModule";
   }
 }
